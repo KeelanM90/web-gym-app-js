75f4fb782a284f1fde127f76c350b8537bc7b9a0
Keelan Murphy <keelanmurphy90@gmail.com>
2017-09-02 18:03:54 +0100

    Added views for adding a class, customised file input

--- controllers/classes.js
+++ controllers/classes.js
@@ -4,10 +4,23 @@ const uuid = require('uuid');
 const accounts = require('./accounts');
 const logger = require('../utils/logger');
 const classStore = require('../models/class-store');
+const dateformat = require('dateformat');
 
 const classes = {
   index(request, response) {
     logger.info('classes rendering');
+    const date = new Date();
+    date.setDate(date.getDate() + (7 - date.getDay()) % 7 + 1);
+    let weekStartingDates = [
+    ];
+    for (let i = 0; i < 12; i++)
+    {
+      weekStartingDates[i]  = {
+        date: dateformat(date, 'dd/mm/yyyy'),
+        readableDate: dateformat(date, 'dddd, dd/mm'),
+      };
+      date.setDate(date.getDate() + 7);
+    }
 
     const loggedintrainer = accounts.getCurrentTrainer(request);
     const classes = classStore.getTrainersClasses(loggedintrainer.id);
@@ -15,6 +28,7 @@ const classes = {
       title: 'Classes',
       trainer: loggedintrainer,
       classes: classes,
+      weeks: weekStartingDates,
     };
     response.render('classes', viewData);
   },
@@ -22,13 +36,29 @@ const classes = {
   addClass(request, response) {
     const trainerId = request.params.trainerid;
 
+    const duration = request.body.duration;
+
     const newClass = {
-      assessmentId: uuid(),
+      classId: uuid(),
       name: request.body.name,
+      difficulty: request.body.difficulty,
+      capacity: request.body.capacity,
+      description: request.body.description,
     };
+
     classStore.addClass(trainerId, newClass);
 
-    response.redirect('/classes');
+    const picture = request.files.picture;
+
+    if (picture != null) {
+      classStore.addPicture(newClass, picture, function () {
+        classStore.store.save();
+        response.redirect('/classes');
+      });
+    } else {
+      classStore.store.save();
+      response.redirect('/classes');
+    }
   },
 
   deleteClass(request, response) {
--- models/class-store.js
+++ models/class-store.js
@@ -3,6 +3,17 @@
 const _ = require('lodash');
 const JsonStore = require('./json-store');
 const logger = require('../utils/logger');
+const cloudinary = require('cloudinary');
+const path = require('path');
+
+try {
+  const env = require('../.data/.env.json');
+  cloudinary.config(env.cloudinary);
+}
+catch (e) {
+  logger.info('You must provide a Cloudinary credentials file - see README.md');
+  process.exit(1);
+}
 
 const classStore = {
 
@@ -24,6 +35,27 @@ const classStore = {
     const classes = this.getTrainersClasses(trainerId);
     _.remove(classes, { classId: classId });
     this.store.save();
+  },
+
+  addPicture(classes, imageFile, response) {
+    if (classes.img != null) {
+      const id = path.parse(member.img);
+      cloudinary.api.delete_resources([id.name], function (result) {
+            console.log(result);
+          }
+      );
+    }
+
+    imageFile.mv('tempimage', err => {
+      if (!err) {
+        cloudinary.uploader.upload('tempimage', result => {
+          console.log(result);
+          classes.img = result.url;
+          this.store.save();
+          response();
+        });
+      }
+    });
   },
 };
 
